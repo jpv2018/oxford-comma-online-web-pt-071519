3117873bec3dbdda8e35454a37afb55b87970311
Jason Viola <jason.viola@mavs.uta.edu>
2019-07-24 20:07:18 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.length > 2 
+    array[0...-1].join(", ") + ", and #{array[-1]}"
+    elsif array.length > 1 
+    array[0..-1].join(" and ")  
+    else
+    array[0..-1].join(", ")
+  end
 end
+
